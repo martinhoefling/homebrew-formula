d575fabc2fb908aaf2c8b7c3aee3646ce6b6f7ef
Martin Hoefling <martin.hoefling@gmx.de>
2018-10-25 17:36:40 +0200

    kns added

---
+++ kns.rb
@@ -0,0 +1,26 @@
+class Kns < Formula
+  desc "Quick Kubernetes Namespace and Context Switchers"
+  homepage "https://github.com/blendle/kns"
+  url "https://github.com/blendle/kns.git", :revision => "9e30d8df96dc864383845ea5a12cd460f33166fa"
+  version "9e30d8df96dc864383845ea5a12cd460f33166fa"
+  head "https://github.com/blendle/kns.git"
+
+  depends_on "fzf"
+  depends_on "kubernetes-cli" => :optional
+
+  def install
+    bin.install "bin/kns"
+    bin.install "bin/ktx"
+    prefix.install "helpers"
+  end
+
+  def caveats; <<~EOS
+    I really recommend this alias:
+      $ alias k=kubectl
+    EOS
+  end
+
+  test do
+    system "which", "kns"
+  end
+end
